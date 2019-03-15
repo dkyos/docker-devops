c654596847920bf809cb0c3578222c4fade925e5
dk77.yun <dk77.yun@samsung.com>
2019-03-16 08:35:30 +0900

    update for dkyos-ml images
    Signed-off-by: dk77.yun <dk77.yun@samsung.com>

---
+++ jupyter_pwd.py
@@ -0,0 +1,10 @@
+from notebook.auth import passwd
+print(passwd())
+
+#/home/dkyos/.jupyter/jupyter_notebook_config.py
+#231 c = get_config()
+#232 c.NotebookApp.ip = '0.0.0.0'
+#233 c.NotebookApp.open_browser = False
+#234 c.NotebookApp.port = 8888
+#235 c.NotebookApp.password = 'sha1:9da11b99a19894'
+
